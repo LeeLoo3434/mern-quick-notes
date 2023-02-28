05b253ea8c67dc9ec39cd6568ce04f839e11e24e
msiller455 <msiller455@gmail.com>
2023-02-28 15:03:22 -0800

    fixes ensureLoggedIn middleware

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(require('./config/checkToken'))
-app.use(require('./config/ensureLoggedIn'))
 
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'))
