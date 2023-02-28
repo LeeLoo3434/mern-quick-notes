b44cda147e941e75252090e896e5be9a3376c13e
msiller455 <msiller455@gmail.com>
2023-02-28 10:40:33 -0800

    finishes part 6

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -21,3 +21,7 @@ export function getUser() {
     const token = getToken()
     return token ? JSON.parse(atob(token.split('.')[1])).user : null
 }
+
+export function logOut() {
+    localStorage.removeItem('token')
+}
