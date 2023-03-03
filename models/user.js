a30f4cbac19b69566f81c4a994767491ac40b55d
LeeLoo3434 <lolarussell347@gmail.com>
2023-03-03 14:58:50 -0800

    MERN stack infrastructure

--- models/user.js
+++ models/user.js
@@ -7,15 +7,15 @@ const SALT_ROUNDS = 6
 const userSchema = new Schema({
     name: {type: String, required: true},
     email: {
-      type: String,
-      unique: true,
-      trim: true,
-      lowercase: true,
-      required: true
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true
     },
     password: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     }
 }, {
     timestamps: true,
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import * as userService from '../../utilities/users-service' 
+import './NavBar.css'
 
 export default function NavBar({ user, setUser }) {
 
@@ -9,9 +10,8 @@ export default function NavBar({ user, setUser }) {
     }
 
     return (
-        <nav>
+        <nav className='nabvar'>
             <Link to="/orders">Order History</Link>
-            &nbsp; | &nbsp;
             <Link to="/orders/new">New Order</Link>
             &nbsp;&nbsp; Welcome, {user.name}
             &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
